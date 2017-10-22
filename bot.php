442e663440c5210376b5286bffbe6aaf527bfdbe
Zttap <32987515+Zttap@users.noreply.github.com>
2017-10-22 19:29:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,11 +1,2 @@
 <?php
 echo "I am a bot";
-curl -X GET \
--H 'Authorization: Bearer {aTAZDqQoK1vF0CgDq0qR+Q8Yse/Bho3K9kattOkyMyoE6gXhhAJJGgXSoHmjSv4XgO1IVXAS2nmX03cbubtTrHjfPL+cWkB0PGb81HFUw1xXithBP40fc2siZWObee6z3ebS8g1yeVavVBjo9ef66AdB04t89/1O/w1cDnyilFU=
-}' \
-https://api.line.me/v1/oauth/verify
-curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {aTAZDqQoK1vF0CgDq0qR+Q8Yse/Bho3K9kattOkyMyoE6gXhhAJJGgXSoHmjSv4XgO1IVXAS2nmX03cbubtTrHjfPL+cWkB0PGb81HFUw1xXithBP40fc2siZWObee6z3ebS8g1yeVavVBjo9ef66AdB04t89/1O/w1cDnyilFU=
-}' \
-...

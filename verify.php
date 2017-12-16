6dd58321c77b9ade4d542f7217e721d73065d3a1
Zttap <32987515+Zttap@users.noreply.github.com>
2017-12-17 01:52:51 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'Q2b0TVZoM5MPcJ3GUNeiiueDlO7Vs5uuggARwpCqWZjR22fmoU8dHsI9+LSB1cYvFOm2tuc0gc2BHABHHrkOt/5yjTizsKRWqFC+/mC4+BQV6v0DYQPIbBF02uJsY5Z36D4cAP5jhu2r59OBXTc6agdB04t89/1O/w1cDnyilFU=
+$access_token = 'qjt61qXecT/hSc+vhEUC4dWJTu2nglzt1DeM8M9TuckNBdF+3XSoE+Tn1oGzAPjjlbhBba5qW9ea+hpIAjKmXJG8ps8cHM4KxO1mUnj+gejLYw2hnRG7hJ66e1yewuXJ7f2jGX6/TQKY6v7/DW/c/gdB04t89/1O/w1cDnyilFU=
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
